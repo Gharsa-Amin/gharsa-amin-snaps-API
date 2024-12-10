b8fcff1bf240aadc1b86707de4092f86c8a1bdc2
Gharsa Amin <ghars0718@gmail.com>
2024-12-09 22:29:19 -0500

    created the id/comments endpoint, used .map loop and find to filter comments based on id, and show only the required variables

--- routes/photos.js
+++ routes/photos.js
@@ -1,3 +1,4 @@
+import { timeStamp } from "console";
 import express from "express";
 import fs from "fs";
 const router = express.Router();
@@ -12,6 +13,7 @@ const photosArray = () => {
 router.get("/", (req, res) => {
 	const photos = photosArray();
 	res.json(photos);
+	res.status(200).send("Successful");
 });
 
 router.get("/:id", (req, res) => {
@@ -35,6 +37,27 @@ router.get("/:id", (req, res) => {
 	};
 
 	res.json(filteredPhoto);
+});
+
+router.get("/:id/comments", (req, res) => {
+	// create a get request with the end point identified followed by a req, res function
+	const { id } = req.params; //use params for id
+	const photos = photosArray(); //calling photosArray which has fs readFileSync of the photo.js file
+	const individualComment = photos.find((photo) => photo.id === id); //running a find function on photo id
+
+	if (!individualComment) {
+		return res.status(404).send("Photo not found");
+	}
+	const filteredComments = individualComment.comments.map((comment) => ({
+		//filtering the individual comments using .map function to identify which variables within the object for the specific endpoint should be used.
+		id: comment.id,
+		name: comment.name,
+		comment: comment.comment,
+		timestamp: comment.timestamp,
+	}));
+
+	// Send the filtered comments as the response
+	res.json(filteredComments); //calling the filteredComment which will show up when the response is sent upon the API end point being called.
 });
 
 export default router;
