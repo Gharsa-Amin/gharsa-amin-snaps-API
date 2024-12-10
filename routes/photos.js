4ed15f69fde0513cfad069f5ca3f25f2446983b4
Gharsa Amin <ghars0718@gmail.com>
2024-12-09 21:25:23 -0500

    created endpoint for handling API requests

--- routes/photos.js
+++ routes/photos.js
@@ -14,6 +14,36 @@ router.get("/", (req, res) => {
 	res.json(photos);
 });
 
-router.get("/");
+router.get("/:id", (req, res) => {
+    console.log(req.params);
+    const {id} = req.params
+    const photosFile = fs.readFileSync("./data/photos.json");
+	const photosParsing = JSON.parse(photosFile); 
+    const individualPhoto = photos.find((photo)) => photo
+}
+
+router.get('/:id' ,(request, response) => {
+    console.log(request.params);
+    const { id } = request.params
+
+    // reading file from file system 
+    const shoesFile = fs.readFileSync('./data/shoes.json');
+    const shoes = JSON.parse(shoesFile);
+
+    const individualShoe = shoes.find((shoe) => shoe.id === id);
+
+    if(!individualShoe) {
+        response.status(404).send('shoe not found')
+        return;
+    }
+
+
+    response.json(individualShoe)
+})
+
+
+
+
+
 
 export default router;
